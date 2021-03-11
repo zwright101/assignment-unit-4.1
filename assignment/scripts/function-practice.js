915d8a6a7d0d686133059e57e9fb4879e40c249b
mbmosman <mbMosman@users.noreply.github.com>
2021-03-11 15:26:35 -0600

    split assignment repos

---
+++ assignment/scripts/function-practice.js
@@ -0,0 +1,88 @@
+console.log('***** Function Practice *****')
+
+// Add the required code to complete the functions below
+// After _each_ function, use a console log to call the function
+// to test it and display the result
+
+// 1. Function to return 'Hello World!'
+function hello() {
+  return 'Hello World!';
+}
+// Call the function to test
+console.log('Test - should say "Hello World!"', hello());
+
+
+// 2. Function to return an personalized hello, for example 'Hello, Jo!'
+function helloName( name ) {
+  return;
+}
+// Remember to call the function to test
+
+
+// 3. Function to add two numbers together & return the result
+function addNumbers( firstNumber ) {
+  // return firstNumber + secondNumber;
+}
+
+
+// 4. Function to multiply three numbers & return the result
+function multiplyThree( ){
+
+}
+
+
+// 5. Function that will return true if a number is positive, 
+//    or greater than zero, and false otherwise
+function isPositive( number ) {
+  if ( number > 0 ){
+    return;
+  }
+    return;
+}
+// Call the function to test each outcome (true & false) 
+// Write a separate console.log statement for each outcome
+console.log( 'isPositive - should say true', isPositive(3) );
+console.log( 'isPositive - should say false', isPositive(0) );
+console.log( 'isPositive - should say false', isPositive(-3) );
+
+
+// 6. Function to return the _last_ item in an array. If the 
+//    array is empty, return `undefined`.
+function getLast( array ) {
+
+}
+
+// 7. Function to find a value in an array. Return true if the 
+//    value is found and false otherwise. Use a loop;
+//    DO NOT use Array.includes, Array.indexOf, or Array.find 
+function find( value, array ){
+  
+}
+
+// ----------------------
+// Stretch Goals
+// ----------------------
+// 8. Function to check if a letter is the first letter in a 
+//    string. Return true if it is, and false otherwise
+function isFirstLetter(letter, string) {
+
+}
+console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
+console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+
+// 9. Function to return the sum of all numbers in an array
+function sumAll( ) {
+  let sum = 0
+  // TODO: loop to add items
+  return sum;
+}
+
+// 10. Function to return a new array of all positive (greater than zero)
+//     numbers contained in an input array. If there are no positive numbers
+//     return an empty array. Note: The input array should not change.
+
+
+
+// 11. Pick a problem from Edabit(https://edabit.com/) or 
+//     CodeWars(https://www.codewars.com/). Then describe it 
+//     here in a comment, write the function, and test it!
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Tier 1 Week 4 - Part 1
+
+This week we'll be pulling together all of the Javascript techniques you've learned thus far as well as practicing with HTML & CSS.
+
+## Topics Covered
+
+- variables
+- arrays
+- conditionals
+- functions
+- HTML & CSS
+
+## Assignment - Function Practice
+
+**REMINDER:** Make sure to answer the Slack discussion question for this week!
+
+Complete the `function-practice.js` file. 
+> Note that the first problem is completed for you as an example.
+
+In this file you will *__fill in__* the missing code to complete the function as described in the comments. (Think of it as similar to fill in the blanks, but we don't show you where the blanks are.) To do this, you may need to add input parameters, return values and/or logic to the function body. For the last few, you'll need to write it all!
+
+After completing each function. Make sure to call (or use) the function in your code (as shown in the example) to test it and make sure it works correctly. For more complex functions, test multiple outcomes to ensure it works correctly in all scenarios. 
+
+## Assignment Submission
+Check in your repo, then turn in your work via the Prime Academy Assignment Application at http://primeacademy.io, as usual and don't hesitate to hit up the Slack channel as needed!
